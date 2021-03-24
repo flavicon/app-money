9a53db705b6a6d17133ef01032c4e0d562591cd0
flavio silva <flavio.devjs@gmail.com>
2021-03-24 16:30:27 -0300

    criação de components, estilo global e icone

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+import { Summary } from '../summary';
+
+export function Dashboard() {
+	
+  return (
+    <Container>
+      <Summary />      
+    </Container>
+  )
+}
---
+++ src/components/summary/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -10rem;
+      
+  div {
+    background: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(text-title);
+
+    header {
+      display: flex;
+      justify-content: space-between;
+      align-itens: center;
+    }
+
+    strong {
+      font-size: 2rem; 
+      font-weight: 500;
+      margin-top: 1rem;
+      display: block;
+    }
+
+    &.highlight-background {
+      background: var(--green);
+      color: var(--shape);
+    }
+  }  
+`
